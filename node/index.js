475f6563cae70aa097775a7023ca685fb05deb91
Noé Krebs <63413892+Sumis34@users.noreply.github.com>
2024-05-25 11:47:30 +0200

    Update index.js

--- node/index.js
+++ node/index.js
@@ -8,5 +8,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port} v1.0.17`);
+  console.log(`Example app listening on port ${port} v1.0.18`);
 });
