c4ccf14e3921d6c2e78f1cfb2c4f09ec7db34559
Noé Krebs <63413892+Sumis34@users.noreply.github.com>
2024-11-20 14:55:32 +0100

    Aktualisieren von index.js

--- fastify/index.js
+++ fastify/index.js
@@ -11,7 +11,7 @@ fastify.get('/', async (request, reply) => {
  */
 const start = async () => {
   try {
-    await fastify.listen({ port: 3000 })
+    await fastify.listen({ port: 3000, host: '0.0.0.0' })
   } catch (err) {
     fastify.log.error(err)
     process.exit(1)
